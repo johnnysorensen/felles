b586dddb6a8700d185917721750d5862602c0ae5
Johnny Sørensen <johnny.sorensen@skatteetaten.no>
2021-01-07 22:06:48 +0100

    laget egen "database" for utvikling. (devlister)

--- src/utils/konstanter.js
+++ src/utils/konstanter.js
@@ -1,2 +1,2 @@
-export const COLLECTION_LISTER = 'lister';
+export const COLLECTION_LISTER = process.env.NODE_ENV === 'development' ? 'devlister' : 'lister';
 export const COLLECTION_LISTE = 'liste';
